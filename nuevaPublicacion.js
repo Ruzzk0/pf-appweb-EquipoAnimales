f03f6c0ca1abb3293cd5568f8f7e63b758bb8d1f
JonathanAstorga <jonathan.astorga245791@potros.itson.edu.mx>
2024-10-29 12:55:18 -0700

    ultimo avance

--- nuevaPublicacion.js
+++ nuevaPublicacion.js
@@ -1,4 +1,4 @@
-document.getElementById("btnPublicar").addEventListener("click", function(event) {
+document.getElementById('btnPublicar').addEventListener("click", function(event) {
     event.preventDefault(); // Evita el envío del formulario para procesarlo en el frontend
 
     // Captura los valores de cada campo
@@ -8,6 +8,12 @@ document.getElementById("btnPublicar").addEventListener("click", function(event)
     const dieta = document.getElementById("dietaAnimalPost").value;
     const habitad = document.getElementById("habitadAnimalPost").value;
     const imagen = document.getElementById("imagenPost").files[0]; // Captura el archivo de imagen
+
+    if (!nombre || !tamaño || !caracteristicas || !dieta || !habitad) {
+        alert("Por favor, completa todos los campos requeridos.");
+        return; // Detiene el flujo si algún campo está vacío
+    }
+
 
     // Guarda los datos en un objeto
     const animalData = {
@@ -19,5 +25,6 @@ document.getElementById("btnPublicar").addEventListener("click", function(event)
         imagen: imagen
     };
 
+    window.location.href = 'principal.html';
     // Aquí puedes hacer lo que necesites con los datos, como enviarlos a un servidor
 });
