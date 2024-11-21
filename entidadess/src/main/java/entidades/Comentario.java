ce718ff4f917ed31c7142e651cd1b538e510c9b2
BasS12009 <149912863+BasS12009@users.noreply.github.com>
2024-11-21 00:41:41 -0700

    Clases
    Entidades echas

--- entidadess/src/main/java/entidades/Comentario.java
+++ entidadess/src/main/java/entidades/Comentario.java
@@ -4,10 +4,76 @@
  */
 package entidades;
 
+import java.time.LocalDateTime;
+
+
 /**
  *
  * @author diana
  */
 public class Comentario {
+    private int id;
+    private String contenido;
+    private LocalDateTime fechaComentario;
+    private Usuario autor;
+    private Publicacion publicacion;
+
+    public Comentario() {
+    }
+
+    public Comentario(int id, String contenido, LocalDateTime fechaComentario, Usuario autor, Publicacion publicacion) {
+        this.id = id;
+        this.contenido = contenido;
+        this.fechaComentario = fechaComentario;
+        this.autor = autor;
+        this.publicacion = publicacion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public LocalDateTime getFechaComentario() {
+        return fechaComentario;
+    }
+
+    public void setFechaComentario(LocalDateTime fechaComentario) {
+        this.fechaComentario = fechaComentario;
+    }
+
+    public Usuario getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Usuario autor) {
+        this.autor = autor;
+    }
+
+    public Publicacion getPublicacion() {
+        return publicacion;
+    }
+
+    public void setPublicacion(Publicacion publicacion) {
+        this.publicacion = publicacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Comentario{" + "id=" + id + ", contenido=" + contenido + ", fechaComentario=" + fechaComentario + ", autor=" + autor + ", publicacion=" + publicacion + '}';
+    }
+   
+    
     
 }
--- entidadess/src/main/java/entidades/Publicacion.java
+++ entidadess/src/main/java/entidades/Publicacion.java
@@ -4,10 +4,64 @@
  */
 package entidades;
 
+import java.time.LocalDateTime;
+
 /**
  *
  * @author diana
  */
 public class Publicacion {
+    private int id;
+    private String contenido;
+    private LocalDateTime fechaPublicacion;
+    private Usuario autor;
+
+    public Publicacion() {
+    }
+
+    public Publicacion(int id, String contenido, LocalDateTime fechaPublicacion, Usuario autor) {
+        this.id = id;
+        this.contenido = contenido;
+        this.fechaPublicacion = fechaPublicacion;
+        this.autor = autor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public LocalDateTime getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    public void setFechaPublicacion(LocalDateTime fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    public Usuario getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Usuario autor) {
+        this.autor = autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Publicacion{" + "id=" + id + ", contenido=" + contenido + ", fechaPublicacion=" + fechaPublicacion + ", autor=" + autor + '}';
+    }
+    
     
 }
